aea6065a5f68b0b0e15f5d5ac4b86f872a270253
Lavy <deckelbaumlavy@gmail.com>
2026-09-03 23:53:54 +0300

    moved getting the button rect out of the function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-document.addEventListener('click', function(event) {
-    const element = document.getElementById('button');
-    const rect = element.getBoundingClientRect();
+const element = document.getElementById('button');
+const rect = element.getBoundingClientRect();
 
+document.addEventListener('click', function(event) {
     if (event.clientX > rect.left & event.clientX < rect.right & event.clientY > rect.top & event.clientY < rect.bottom) {
         console.log("Pressed!")
     }
