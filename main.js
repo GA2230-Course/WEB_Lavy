d443f97fe389087ca1f51af6c9efb5097ac2c8d2
Lavy <deckelbaumlavy@gmail.com>
2026-09-04 00:13:31 +0300

    made it so when you click the button the page header disapears and reapears after 1 second.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const element = document.getElementById('button');
-const rect = element.getBoundingClientRect();
+const button = document.getElementById('button');
+const rect = button.getBoundingClientRect();
+
+const text = document.getElementById('button-text');
 
 document.addEventListener('click', function(event) {
     if (event.clientX > rect.left & event.clientX < rect.right & event.clientY > rect.top & event.clientY < rect.bottom) {
-        setTimeout(() => {
-            console.log("Pressed!")
-        }, 1000)
+        text.style.visibility = "hidden";
+        setTimeout(() => {text.style.visibility = "visible";}, 1000)
     }
 });
